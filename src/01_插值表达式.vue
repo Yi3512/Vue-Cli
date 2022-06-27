e3188cfa4d8f9a23a2b51f402d2aa2bd3003cf8d
mhh <1147905003@qq.com>
2022-06-28 06:02:37 +0800

    feat:init

---
+++ src/01_插值表达式.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <div>
+    {{ msg }}
+    <!-- Property or method "msg" is not defined -->
+    <!-- template 里的变量是需要定义的 -->
+    <h1>{{obj.name}}</h1>
+     <h1>{{obj.age}}</h1>
+     <h1>{{obj.age >= 18 ? '成年':'未成年'}}</h1>
+  </div>
+</template>
+
+<script>
+// 插值表达式
+// 语法: {{ 表达式 }} 
+// 作用可以把表达式的返回值 直接 插入到dom 标签中
+export default {
+  name: 'App',
+  data () { // template 里的变量是需要 在data 函数里返回的对象里定义
+    return { // 固定格式
+      msg: 'Hello Vue',
+      obj: {
+        name: 'Vue',
+        age: 16
+      }
+    }
+  }
+  
+}
+</script>
