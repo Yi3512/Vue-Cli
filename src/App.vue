245bbc448233ecee31a12a82813c7896ccd77c79
Yi3512 <2399283079@qq.com>
2022-06-28 19:56:51 +0800

    7.翻转字符串案例

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div>
-    nothing
+    <h1>{{ msg }}</h1>
+    <button @click="btn">逆转世界</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  data () { 
-    return { 
-      
-    }
-  }
-}
+  naem:'App',
+  data() {
+    return {
+      msg: 'Hello, world!',
+    };
+  },
+  methods: {
+    btn() {
+      this.msg = this.msg.split('').reverse().join('');
+    },
+  },
+};
 </script>
+
+<style></style>
