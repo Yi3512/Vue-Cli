69f0747370522d051bc292ffde941115bc5dd94f
Yi3512 <2399283079@qq.com>
2022-06-28 19:42:33 +0800

    3.事件绑定

---
+++ src/03_事件绑定.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>你要购买商品的数量: {{ count }}</p>
+    <!-- 1. 绑定事件
+  语法: v-on:事件名="少量代码"
+  语法: v-on:事件名="methods里函数名"
+  语法: v-on:事件名="methods里函数名(值)"
+  语法: @事件名="methods里函数名"
+  -->
+    <button v-on:click="count = count + 1">+1</button>
+    <button v-on:click="addFn">+1</button>
+    <button @click="addCountFn(5)">+5</button>
+    <button @click="subFn">减少1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    addFn() {
+      this.count++;
+    },
+    addCountFn(num) {
+      this.count += num;
+    },
+    subFn() {
+      this.count--;
+    },
+  },
+};
+</script>
+
+<style></style>
