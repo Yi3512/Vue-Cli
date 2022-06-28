b630ae960d69a2710d79fa7d8f87b53d0cce547c
Yi3512 <2399283079@qq.com>
2022-06-28 20:32:04 +0800

    模拟v-model

--- src/08_01_v-model的基本使用.vue
+++ src/08_01_v-model的基本使用.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
-    
+    <input type="text" v-model="val" />
+    <!--
+   实现v-model
+   数据到视图
+   视图到数据
+   1. value  v-bind
+   2. 输入事件 输入过程中 实时拿到dom input 里的value
+   -->
+    <br />
+    <input type="text" v-bind:value="val1" @input="inputFn" />
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: 'App',
+  data() {
+    return {
+      val: '',
+      val1: '',
+    };
+  },
+  methods: {
+    inputFn(e) {
+      this.val1 = e.target.value;
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style scoped></style>
