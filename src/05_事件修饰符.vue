bee0235c10c2102ffebd0ec5223bd2021d970b7c
Yi3512 <2399283079@qq.com>
2022-06-28 19:49:20 +0800

    5.事件修饰符

---
+++ src/05_事件修饰符.vue
@@ -0,0 +1,22 @@
+<template>
+  <div @click="fath">
+    <button @click.stop="btn">stop阻止冒泡</button>
+    <a href="http://www.baidu.com" @click.prevent="btn">prevent阻止冒泡</a>
+    <button @click.once="btn">once程序运行期间, 只触发一次事件处理函数</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    fath() {
+      console.log('触发冒泡了');
+    },
+    btn() {
+      console.log('成功');
+    },
+  },
+};
+</script>
+
+<style></style>
