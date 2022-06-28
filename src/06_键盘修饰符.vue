4965bd73008f86804a805c8fb94e35f0b5698a15
Yi3512 <2399283079@qq.com>
2022-06-28 19:51:26 +0800

    6.键盘修饰符

---
+++ src/06_键盘修饰符.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input type="text" @keydown.enter="enter">
+    <hr>
+    <input type="text" @keydown.esc="esc">
+  </div>
+</template>
+
+<script>
+export default {
+    methods:{
+        enter(){
+            console.log('回车触发');
+        },
+        esc(){
+            console.log('esc触发');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
